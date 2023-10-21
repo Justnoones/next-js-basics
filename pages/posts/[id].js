39560d49a0172998caa14d28cdf1cdb391bc028a
Justnoones <yanw97841@gmail.com>
2023-10-21 11:56:48 +0630

    page-path-depends-on-external-data|dynamic-routes

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,13 +1,31 @@
-import { useRouter } from "next/router"
+import Layout from "/components/Layout";
+import { getAllPostIds, getPostData } from "/lib/posts";
 
-const Posts = () => {
-  const router = useRouter();
+export default ({ postData }) => {
   return (
-    // url http://localhost:3000/posts/1 = { id : [1]}
-    // url http://localhost:3000/posts/1/2 = { id : [1, 2]}
-    // url http://localhost:3000/posts/1/2/3 = { id : [1, 2, 3]}
-    <div>{router.query.id}</div>
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+    </Layout>
   )
 }
 
-export default Posts
+export const getStaticPaths = async () => {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false
+  };
+}
+
+export const getStaticProps = async ({ params }) => {
+  const postData = getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
